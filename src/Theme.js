9179a87ee236041aed6a4378ca49efe01ca2a9db
Daniel Tavares <daniel.tavares.1301@gmail.com>
2022-04-13 22:16:58 -0300

    create global theme

---
+++ src/Theme.js
@@ -0,0 +1,23 @@
+import { createTheme } from "@mui/material/styles";
+
+// A custom theme for this app
+const theme = createTheme({
+  palette: {
+    background: {
+      default: "#F0F0F0",
+    },
+    secondary: {
+      default: "#fff",
+    },
+    error: {
+      main: "#C70000",
+    },
+    button: {
+      add: "green",
+      delete: "red",
+      update: "blue",
+    },
+  },
+});
+
+export default theme;
