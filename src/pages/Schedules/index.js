0cafa2220c58fccfcaa8344c10a23d6c877b9da7
Daniel Tavares <daniel.tavares.1301@gmail.com>
2022-04-20 17:47:00 -0300

    feature: list sorted schedule

--- src/pages/Schedules/index.js
+++ src/pages/Schedules/index.js
@@ -1,21 +1,27 @@
 import { useEffect, useState } from "react";
 
 const Schedules = () => {
-  const [data, setData] = useState();
+  const [data, setData] = useState([]);
+
+  const inOrder = data.reduce((object, item) => {
+    object[item.appointmentDate] = data
+      .filter((value) => value.appointmentDate === item.appointmentDate)
+      .sort(
+        (compare1, compare2) =>
+          compare1.appointmentTime - compare2.appointmentTime
+      );
+    return object;
+  }, {});
+
   useEffect(() => {
     setData(JSON.parse(localStorage.getItem("data")));
-    console.log(data);
   }, []);
+
   return (
     <div>
-      {data.map((item) => (
-        <>
-          <div key={item._id}>
-            {item.appointmentDate}
-            {item.appointmentTime}
-          </div>
-        </>
-      ))}
+      <button onClick={() => console.log(inOrder)}>
+        Mostra dados no console
+      </button>
     </div>
   );
 };
