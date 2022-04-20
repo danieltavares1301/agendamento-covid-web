999dfac2f40a0dedc4b055cabacdbb8158356353
Daniel Tavares <daniel.tavares.1301@gmail.com>
2022-04-19 21:14:34 -0300

    feature: add business rules in register

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -14,68 +14,79 @@ import axios from "axios";
 
 const Home = () => {
   const [data, setData] = useState([]);
-  const [dataFromLST, setDataFromLST] = useState([]);
 
   const schema = Yup.object().shape({
-    name: Yup.string().required("Name is required"),
-    email: Yup.string().email().required("Email is required"),
-    appointmentHour: Yup.number().required("appointmentHour is required"),
+    name: Yup.string().required("O campo de nome precisa ser preenchido!"),
+    birthDate: Yup.date().required(
+      "O campo de data de nascimento precisa ser preenchido!"
+    ),
+    appointmentTime: Yup.number().required("O campo precisa ser preenchido!"),
     appointmentDate: Yup.date().required("appointmentDate is required"),
     isFinished: Yup.boolean().default(false),
     description: Yup.string(),
   });
 
+  // pega dados do BD ao entrar na aplicação
   useEffect(() => {
     axios
       .get("http://localhost:4000/schedule")
       .then((res) => setData(res.data));
   }, []);
+
+  // quando a state data for alterada, ela é colocada novamente no localStorage
   useEffect(() => {
     localStorage.setItem("dados", JSON.stringify(data));
-    setDataFromLST(JSON.parse(localStorage.getItem("dados")));
   }, [data]);
 
-  //console.log(dataFromLST);
-
-  const onSubmit = (values) => {
+  const onSubmit = async (values) => {
     //pega quantidade de vezes que aquele dia foi cadastrado
-    const dias = dataFromLST.filter(
+    const dias = data.filter(
       (item) => item.appointmentDate === values.appointmentDate.toISOString()
     ).length; // Se for igual a data, pega a quantidade de datas
 
     //pega quantidade de horarios que sejam igual ao que o usuário pretende cadastrar
-    const horas = dataFromLST
+    const horas = data
       .filter(
         (item) => item.appointmentDate === values.appointmentDate.toISOString()
       ) // Se for igual a data
       .filter(
-        (item) => item.appointmentHour.toString() === values.appointmentHour
+        (item) => item.appointmentTime.toString() === values.appointmentTime
       ).length; // se for igual ao horário
 
     if (dias < 20) {
       if (horas < 2) {
-        console.log("tem vaga");
+        return await axios
+          .post("http://localhost:4000/schedule", {
+            name: values.name,
+            birthDate: values.birthDate,
+            appointmentDate: values.appointmentDate,
+            appointmentTime: values.appointmentTime,
+          })
+          .then((response) => setData([...data, response.data.schedule])) //.schedule pois o backend também retorna uma messagem dentro de data
+          .then(() =>
+            alert(
+              `agendamento do paciente ${values.name} foi realizado com sucesso!`
+            )
+          )
+          .catch((error) => console.erro(error.message));
       } else {
-        console.log("HORARIO NAO tem vaga");
+        alert("HORÁRIO NÃO possui vagas disponíveis!");
       }
     } else {
-      console.log("DIA NAO tem vaga");
+      alert("DIA NÃO possui vagas disponíveis!");
     }
-
-    //console.log(dias, horas);
   };
 
   return (
     <div>
-      <h2>Formik</h2>
       <Formik
         onSubmit={onSubmit}
         validationSchema={schema}
         initialValues={{
           name: "",
-          email: "",
+          birthDate: "",
           appointmentDate: "",
-          appointmentHour: "",
+          appointmentTime: "",
           isFinished: "",
           description: "",
         }}
@@ -84,31 +95,27 @@ const Home = () => {
           <Form>
             <div>
               <label htmlFor="name">Nome</label>
+              <br />
               <Field id="name" name="name" type="text" />
               <ErrorMessage component="div" name="name" />
             </div>
-            <div>
-              <label htmlFor="email">email</label>
-              <Field id="email" name="email" type="text" />
-              <ErrorMessage component="div" name="email" />
-              <br />
-            </div>
-            <label htmlFor="appointmentHour">Schedule</label>
-            <Field
-              as="select"
-              name="appointmentHour"
-              id="appointmentHour"
-              type="number"
-            >
-              {[8, 9, 10, 11, 12, 13, 14, 15, 16, 17].map((item) => (
-                <option value={item} key={item}>
-                  {item}
-                </option>
-              ))}
+            <label htmlFor="birthDate">Data de nascimento</label>
+
+            <Field id="birthDate" name="birthDate">
+              {() => (
+                <DatePicker
+                  selected={values.birthDate}
+                  onChange={(birthDate) =>
+                    setFieldValue("birthDate", birthDate)
+                  }
+                  name="birthDate"
+                />
+              )}
             </Field>
+
             <br />
 
-            <label htmlFor="appointmentDate">date</label>
+            <label htmlFor="appointmentDate">data de agendamento</label>
 
             <Field id="appointmentDate" name="appointmentDate">
               {() => (
@@ -121,6 +128,24 @@ const Home = () => {
                 />
               )}
             </Field>
+            <br />
+            <label htmlFor="appointmentTime">horário de agendamento</label>
+            <Field
+              as="select"
+              name="appointmentTime"
+              id="appointmentTime"
+              type="number"
+            >
+              {
+                // lista de horários de atendimento durante o dia
+                [8, 9, 10, 11, 12, 13, 14, 15, 16, 17].map((item) => (
+                  <option value={item} key={item}>
+                    {`${item}:00`}
+                  </option>
+                ))
+              }
+            </Field>
+            <br />
 
             <button type="submit">submit</button>
           </Form>
