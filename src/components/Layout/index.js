b4a9f87fcd4d2e147d0d48febeb885c363afc2a7
daniel tavares <daniel.tavares.1301@gmail.com>
2022-04-20 10:58:56 -0300

    feature: add paths to buttons in Layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -10,60 +10,68 @@ import {
   ListItemText,
 } from "@mui/material";
 import { Outlet } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-const Layout = () => (
-  <Box
-    sx={{ display: "flex" }}
-    style={{
-      backgroundColor: "#F0F0F0",
-      minHeight: "100vh",
-      maxHeight: "100%",
-      minWidth: "100%",
-    }}
-  >
-    <AppBar
-      position="fixed"
-      sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
-      style={{ backgroundColor: "#fff" }}
+const Layout = () => {
+  const navigate = useNavigate();
+  return (
+    <Box
+      sx={{ display: "flex" }}
+      style={{
+        backgroundColor: "#F0F0F0",
+        minHeight: "100vh",
+        maxHeight: "100%",
+        minWidth: "100%",
+      }}
     >
-      <Toolbar>
-        <Typography
-          variant="h6"
-          noWrap
-          component="div"
-          style={{ color: "black" }}
-        >
-          Schedule for vaccination
-        </Typography>
-      </Toolbar>
-    </AppBar>
-    <Box sx={{ display: "flex" }}>
-      <Drawer
-        sx={{
-          width: 240,
-          "& .MuiDrawer-paper": {
-            width: 240,
-            boxSizing: "border-box",
-          },
-        }}
-        variant="permanent"
-        anchor="left"
+      <AppBar
+        position="fixed"
+        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        style={{ backgroundColor: "#fff" }}
       >
-        <Toolbar />
-        <List>
-          {["Home", "Schedules"].map((item, index) => (
-            <ListItem button key={index}>
-              <ListItemText primary={item} />
+        <Toolbar>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            style={{ color: "black" }}
+          >
+            Schedule for vaccination
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box sx={{ display: "flex" }}>
+        <Drawer
+          sx={{
+            width: 240,
+            "& .MuiDrawer-paper": {
+              width: 240,
+              boxSizing: "border-box",
+            },
+          }}
+          variant="permanent"
+          anchor="left"
+        >
+          <Toolbar />
+          <List>
+            <ListItem button>
+              <ListItemText primary="Home" onClick={() => navigate("/")} />
             </ListItem>
-          ))}
-        </List>
-      </Drawer>
+            <ListItem button>
+              <ListItemText
+                primary="Schedules"
+                onClick={() => navigate("/Schedules")}
+              />
+            </ListItem>
+          </List>
+        </Drawer>
+      </Box>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Toolbar />
+        <Outlet />
+      </Box>
     </Box>
-    <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-      <Toolbar />
-      <Outlet />
-    </Box>
-  </Box>
-);
+  );
+};
 
 export default Layout;
