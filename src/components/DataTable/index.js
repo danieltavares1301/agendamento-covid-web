cb66661420cd411adf704b164cf5e3f57671671f
Daniel Tavares <daniel.tavares.1301@gmail.com>
2022-04-24 14:32:31 -0300

    refactor: remove and add comments

--- src/components/DataTable/index.js
+++ src/components/DataTable/index.js
@@ -1,14 +1,14 @@
-import React, { useState, useContext } from "react";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import { Button, TextField } from "@mui/material";
-import api from "../../services/api";
-import { AppContext } from "../../AppContextProvider";
+import React, { useState, useContext } from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { Button, TextField } from '@mui/material';
+import api from '../../services/api';
+import { AppContext } from '../../AppContextProvider';
 
 const DataTable = ({ list, appointmentDate }) => {
   const [, loadingData] = useContext(AppContext);
@@ -16,32 +16,15 @@ const DataTable = ({ list, appointmentDate }) => {
 
   // atualiza valor do status e da descrição
   const updateValues = async (id, description) => {
+    // neste path, o atendimento é finalizado apenas com o id do agendamento
     await api
       .put(`/serviceFinished/${id}`, {
         description: description,
       })
       .then(() => loadingData())
-      .catch(() => alert("Um erro inesperado ocorreu!"));
+      .catch(() => alert('Um erro inesperado ocorreu!'));
 
     return window.location.reload(true);
-
-    // se não existisse banco de dados, seria mudado dessa forma:
-    //
-    //   const updateStatus = data
-    //     .filter((value) => value._id === id && value.isFinished === false) // pega apenas o objeto que for igual o id passado
-    //     .map((item) => {
-    //     // muda valor do status para true e nova descrição, e os outros itens do obj continuam os mesmo
-    //        if (item._id === id) {
-    //          item.isFinished = true;
-    //          item.description = description;
-    //        }
-    //        return item;
-    //      });
-    // // adiciona itens antigos diferentes do que foi alterado, e também adiciona o alterado.
-    // setData([data.filter((value) => value._id !== id), ...updateStatus].flat());
-    // // altera data no localStorage
-    // return localStorage.setItem("data", JSON.stringify(data));
-    // // refresh para reorganização de dados
   };
   return (
     <>
@@ -59,11 +42,11 @@ const DataTable = ({ list, appointmentDate }) => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {list.map((row) => (
+            {list.map(row => (
               <TableRow
                 key={row._id}
                 sx={{
-                  "&:last-child td, &:last-child th": { border: 0 },
+                  '&:last-child td, &:last-child th': { border: 0 },
                 }}
               >
                 <TableCell align="center" component="th" scope="row">
@@ -76,13 +59,18 @@ const DataTable = ({ list, appointmentDate }) => {
                   {new Date(row.birthDate).toLocaleDateString()}
                 </TableCell>
                 <TableCell align="center" component="th" scope="row">
-                  {row.isFinished === false ? (
-                    <Button onClick={() => updateValues(row._id, description)}>
-                      Atender Paciente
-                    </Button>
-                  ) : (
-                    "Paciente Atendido"
-                  )}
+                  {
+                    // se o atendimento ainda não estiver finalizado, irá aparecer a opção de finalizar
+                    row.isFinished === false ? (
+                      <Button
+                        onClick={() => updateValues(row._id, description)}
+                      >
+                        Atender Paciente
+                      </Button>
+                    ) : (
+                      'Atendimento finalizado'
+                    )
+                  }
                 </TableCell>
                 {row.isFinished === false ? (
                   <TableCell align="center" component="th" scope="row">
@@ -91,7 +79,7 @@ const DataTable = ({ list, appointmentDate }) => {
                       placeholder="Adicione uma descrição antes de clicar em ATENDER PACIENTE"
                       multiline
                       maxRows={4}
-                      onChange={(event) => setDescription(event.target.value)}
+                      onChange={event => setDescription(event.target.value)}
                     />
                   </TableCell>
                 ) : (
